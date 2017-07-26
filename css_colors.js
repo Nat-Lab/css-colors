e2261a1b7c36da3fc32fe814ec3d5a7e744c9203
NAT <nat@magicnat.com>
2017-07-26 17:36:45 +0800

    no == decr check needed.

--- css_colors.js
+++ css_colors.js
@@ -6,25 +6,6 @@ if (args.length < 2) {
   process.exit(1);
 }
 
-var getColorByUrl = url => new Promise((res, rej) => {
-  getcss(url, {timeout: 5000})
-    .then(
-      src => res(
-        css.parse(src.css).stylesheet.rules
-          .filter(r => r.declarations)
-          .map(
-            r => r.declarations.filter(
-              d => d.type == 'declaration' &&
-              [ 'color',
-                'background-color' ].some(c => d.property == c)
-            )
-          )
-          .reduce((d1, d2) => d1.concat(d2))
-          .map(d => d.value)
-      )
-    ).catch(rej);
-});
-
 var urls = fs.readFileSync(args[0], 'utf8').split('\n');
 var cs = [];
 
@@ -36,12 +17,32 @@ var fectchColors = function (urls, i, n) {
       return re_rslt ? re_rslt[0] : null;
     }).filter(c => c != null)));
     return;
-  };
-  getColorByUrl(urls[i]).then(c => {
-    console.log(`[${i + 1}/${n}] ${urls[i]}`)
-    cs = cs.concat(c);
-    fectchColors(urls, i+1, n);
-  }).catch(e => fectchColors(urls, i+1, n));
+  }
+
+  (url => new Promise((res, rej) => {
+    getcss(url, {timeout: 5000})
+      .then(
+        src => res(
+          css.parse(src.css).stylesheet.rules
+            .filter(r => r.declarations)
+            .map(
+              r => r.declarations.filter(
+                d => [ 'color',
+                       'background-color' ].some(c => d.property == c)
+              )
+            )
+            .reduce((d1, d2) => d1.concat(d2))
+            .map(d => d.value)
+        )
+      )
+      .catch(rej);
+  }))(urls[i])
+    .then(c => {
+      console.log(`[${i + 1}/${n}] ${urls[i]}`);
+      cs = cs.concat(c);
+      fectchColors(urls, i+1, n);
+    })
+    .catch(e => fectchColors(urls, i+1, n));
 };
 
 fectchColors(urls, 0, urls.length - 1);
